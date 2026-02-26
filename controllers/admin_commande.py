26a23c2dbfbce16bbbc49b51e24a0b6ecfdce240
gmathix <smegmuscarlsen@gmail.com>
2026-02-26 15:29:11 +0100

    admin commande show + basic client coordonnees

--- controllers/admin_commande.py
+++ controllers/admin_commande.py
@@ -18,16 +18,45 @@ def admin_index():
 def admin_commande_show():
     mycursor = get_db().cursor()
     admin_id = session['id_user']
-    sql = '''      '''
 
-    commandes=[]
+    sql = '''
+        SELECT commande.id_commande AS id_commande,
+                login AS login,
+                date_achat AS date_achat,
+                SUM(quantite) AS nbr_velos,
+                SUM(prix) AS prix_total,
+                libelle AS libelle
+        FROM commande
+        INNER JOIN utilisateur ON commande.utilisateur_id = utilisateur.id_utilisateur
+        INNER JOIN etat ON commande.id_etat = etat.id_etat
+        RIGHT JOIN ligne_commande ON commande.id_commande = ligne_commande.id_commande
+            GROUP BY login, date_achat, libelle, commande.id_commande
+  '''
+    mycursor.execute(sql)
+    commandes=mycursor.fetchall()
+
 
     velos_commande = None
     commande_adresses = None
     id_commande = request.args.get('id_commande', None)
-    print(id_commande)
+
+
     if id_commande != None:
-        sql = '''    '''
+        sql = '''  
+            SELECT utilisateur.nom AS nom,
+                quantite AS quantite,
+                prix AS prix,
+                (quantite * prix) AS prix_ligne
+                
+                    
+            FROM ligne_commande
+            INNER JOIN commande ON ligne_commande.id_commande = commande.id_commande
+            INNER JOIN utilisateur ON commande.utilisateur_id = utilisateur.id_utilisateur
+            WHERE ligne_commande.id_commande = %s  
+              '''
+        mycursor.execute(sql, id_commande)
+        velos_commande = mycursor.fetchall()
+
         commande_adresses = []
     return render_template('admin/commandes/show.html'
                            , commandes=commandes
